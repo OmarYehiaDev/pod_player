7ef3271844395b83db3f50ad39e5951e2c0a40db
Omar Yehia <omaryahiatawfiek@gmail.com>
2024-08-06 03:39:05 +0300

    Update `pod_video_quality_controller.dart`

--- lib/src/controllers/pod_video_quality_controller.dart
+++ lib/src/controllers/pod_video_quality_controller.dart
@@ -54,7 +54,8 @@ class _PodVideoQualityController extends _PodVideoController {
     final urls0 = urls;
 
     ///has issues with 240p
-    urls0?.removeWhere((element) => element.quality == 240);
+    /// Commenting this line for the moment..
+    // urls0?.removeWhere((element) => element.quality == 240);
 
     ///has issues with 144p in web
     if (kIsWeb) {
